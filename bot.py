eee98b3841ddb9865946d4fc0598d82d0f9b132f
materufino <rufinomateo99@gmail.com>
2023-09-06 18:56:11 +0200

    no message

--- bot.py
+++ bot.py
@@ -1,14 +1,1 @@
-import random
-
-def run():
-  while True:
-    pregunta = input("¿Qué quieres saber? ")
-    respuesta = random.choice([
-      "No lo sé",
-      "Eso es una pregunta difícil",
-      "¡No puedo creer que me hayas preguntado eso!"
-    ])
-    print(respuesta)
-
-if __name__ == "__main__":
-  run()
+print("Hello World")
